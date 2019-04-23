0b4f1de6622989fd59e11b138ff80a69f391a299
Aleksandr <Aleksandr@Aleksandr-PC.local>
2019-04-23 23:21:24 +0700

    initial project version

---
+++ app.js
@@ -0,0 +1,19 @@
+var main = function () {
+
+
+    var bodyVersionId = $('.bodyVersionId').val();
+    
+    var displayFrameId = '0' + parseInt("00101", 2);
+    var manufacturerId = $('.manufacturerId').val();
+    var serialNumber = 1; 
+    var colorId = $('.colorId').val();;
+    var manufacturingDate = $('.year').val() + $('.month').val();
+    console.log();
+    console.log(manufacturingDate.toString(16));
+
+    var gatewayId =  bodyVersionId.toString(10) + "." + displayFrameId.toString(10) + "." + manufacturerId + colorId + "." + parseInt(manufacturingDate, 10).toString(16) + serialNumber;
+    $('.results').html(gatewayId);
+    return console.log(gatewayId);
+};
+
+$(document).ready(main);
