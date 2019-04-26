d397f731531f90518092175870f2263525f1f8d2
Aleksandr <Aleksandr@Aleksandr-PC.local>
2019-04-26 23:29:01 +0700

    add datepicker, changed input for body id, changed manufacturingDate variable meaning

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
+const picker = datepicker('#datepicker', {
+  formatter: (input, date, instance) => {
+    const value = date.toLocaleDateString()
+    input.value = value // => '1/1/2099'
+  }
+})
+
+
 function getGatewaySerialNumber () {
     var bodyVersionId = $('.bodyVersionId').val();
     var displayFrameId = '0' + parseInt("00101", 2);
     var manufacturerId = $('.manufacturerId').val();
     var serialNumber = 1; 
     var colorId = $('.colorId').val();;
-    var manufacturingDate = $('.year').val() + $('.month').val();
-    console.log();
-    console.log(manufacturingDate.toString(16));
+    var manufacturingDate = ($('#datepicker').val().split('.').join("").substring(2) 
+                            + 
+                            $('#datepicker').val().split('.').join("").substring(2).substring(0, 2))
+                            .substring(4).toString(16);
 
-    var gatewayId =  bodyVersionId.toString(10) + "." + displayFrameId.toString(10) + "." + manufacturerId + colorId + "." + parseInt(manufacturingDate, 10).toString(16) + serialNumber;
+    var gatewayId =  bodyVersionId.toString(10) + "." + displayFrameId.toString(10) + "." + manufacturerId + colorId + "." + parseInt(manufacturingDate, 10).toString(16) + 0 + serialNumber;
     $('.results').html(gatewayId);
     return console.log(gatewayId);
 };
 
-$( function() {
-    $( "#datepicker" ).datepicker();
-  } );
 
