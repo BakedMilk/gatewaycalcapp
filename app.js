639a9315f8e1491330a7c6063c2549e6b10eda09
Aleksandr <Aleksandr@Aleksandr-PC.local>
2019-05-02 13:21:48 +0700

    add table

--- app.js
+++ app.js
@@ -10,16 +10,22 @@ function getGatewaySerialNumber () {
     var bodyVersionId = $('.bodyVersionId').val();
     var displayFrameId = '0' + parseInt("00101", 2);
     var manufacturerId = $('.manufacturerId').val();
-    var serialNumber = 1; 
+    var serialNumber = $('.serialNumber').val(); 
     var colorId = $('.colorId').val();;
     var manufacturingDate = ($('#datepicker').val().split('.').join("").substring(2) 
                             + 
                             $('#datepicker').val().split('.').join("").substring(2).substring(0, 2))
                             .substring(4).toString(16);
 
-    var gatewayId =  bodyVersionId.toString(10) + "." + displayFrameId.toString(10) + "." + manufacturerId + colorId + "." + parseInt(manufacturingDate, 10).toString(16) + 0 + serialNumber;
-    $('.results').html(gatewayId);
-    return console.log(gatewayId);
+        for (var i = 1; i < serialNumber; i++) {
+           var gatewayId =  bodyVersionId.toString(10) + "." + displayFrameId.toString(10) + "." + manufacturerId + colorId + "." + parseInt(manufacturingDate, 10).toString(16) + 0 + i++;
+            $('.results').html(gatewayId);
+            return console.log(gatewayId); 
+        }
+
+
+
+    
 };
 
 
