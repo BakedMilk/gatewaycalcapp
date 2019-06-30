703c1687911c641ba19043ef3d4b9e3837509f5d
Aleksandr <Aleksandr@Aleksandr-PC-2.local>
2019-07-01 00:04:01 +0700

    trying to add cycle for append, not working

--- app.js
+++ app.js
@@ -10,19 +10,30 @@ function getGatewaySerialNumber () {
     var bodyVersionId = $('.bodyVersionId').val();
     var displayFrameId = '0' + parseInt("00101", 2);
     var manufacturerId = $('.manufacturerId').val();
+
+    var isGazePoint =  $('#isGazePoint').val();
+
     var serialNumber = $('.serialNumber').val(); 
-    var colorId = $('.colorId').val();;
+    var colorId = $('.colorId').val();
     var manufacturingDate = ($('#datepicker').val().split('.').join("").substring(2) 
                             + 
                             $('#datepicker').val().split('.').join("").substring(2).substring(0, 2))
                             .substring(4).toString(16);
+    console.log(isGazePoint);   
+       
+    var gatewayId =  
+    bodyVersionId.toString(10) 
+    + "." 
+    + displayFrameId.toString(10) 
+    + "." 
+    + manufacturerId + colorId 
+    + "." 
+    + parseInt(manufacturingDate, 10).toString(16);
+      
 
-        for (var i = 1; i < serialNumber; i++) {
-           var gatewayId =  bodyVersionId.toString(10) + "." + displayFrameId.toString(10) + "." + manufacturerId + colorId + "." + parseInt(manufacturingDate, 10).toString(16) + 0 + i++;
-            $('.results').html(gatewayId);
-            return console.log(gatewayId); 
-        }
-
+    for (i = 1; i < serialNumber; i++) {
+    $('.results').append(gatewayId) 
+    };
 
 
     
